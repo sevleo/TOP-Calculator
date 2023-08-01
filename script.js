7829c547b2a42ba3461d11687b6c07352351060d
Seva <seva.leonov@hotmail.com>
2023-08-01 14:24:42 +0700

    Add round-up

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ buttons.forEach(button => button.addEventListener('click', captureValue));
 let leftOperand = '0';
 let operator = '';
 let rightOperand = '';
+let overrideLeftOperand = false;
 
 function captureValue(e) {
     // Capture left operand
     if (e.target.classList.contains('operand') && operator === '') {
-        leftOperand = formOperand(e, leftOperand);
+        leftOperand = formOperand(e, leftOperand, overrideLeftOperand);
+        overrideLeftOperand = false;
     }
 
     // Capture operator
@@ -26,24 +28,27 @@ function captureValue(e) {
         rightOperand = formOperand(e, rightOperand);
     }
 
+    // Calculate
     if (e.target.classList.contains('equal') && rightOperand !== '') {
-        leftOperand = performCalculus(leftOperand, operator, rightOperand);
+        leftOperand = calculate(leftOperand, operator, rightOperand);
         operator = '';
         rightOperand = '';
+        overrideLeftOperand = true;
     }
 
     screen.textContent = `${leftOperand} ${operator} ${rightOperand}`;
     
 }
 
-function formOperand(e, operand) {
-    if (operand === '' || operand === '0') {
+function formOperand(e, operand, overrideLeftOperand) {
+    if (operand === '' || operand === '0' || overrideLeftOperand === true) {
         if (e.target.classList.contains('dot')) {
             operand = '0.'; // To ensure zero goes before decimal if user enters decimal
         }
         else {
             operand = e.target.innerText; // To ensure only one zero in a number
         }
+        
     }
     else {
         if (operand.includes('.') && e.target.classList.contains('dot')) {
@@ -56,7 +61,7 @@ function formOperand(e, operand) {
     return operand;
 }
 
-function performCalculus(leftOperand, operator, rightOperand) {
+function calculate(leftOperand, operator, rightOperand) {
     const leftNumber = Number(leftOperand);
     const rightNumber = Number(rightOperand);
     switch (operator) {
